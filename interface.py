15e7a41f4f4460bc7ee52def27c97cc10a94981e
maryszmary <masha.shejanova@gmail.com>
2016-12-11 03:35:04 +0300

    minimal changes. todo: do smthng with test names and id

--- interface.py
+++ interface.py
@@ -7,6 +7,7 @@ import sqlite3
 
 
 class TasksDB():
+    '''the db fith tests and users'''
 
     def __init__(self, name):
         self.name = name
@@ -18,7 +19,10 @@ class TasksDB():
         cur.execute('SELECT * FROM tests WHERE language = ?', (lang, ))
         results = cur.fetchall()
         db.close()
-        return results     
+        return results   
+
+    def get_task(self, lang, name):
+        pass  
 
 
 db = TasksDB('tasks.db')
@@ -27,14 +31,20 @@ app = Flask(__name__, static_folder=u"./static")
 
 @app.route('/langtests/guest', methods=['GET', 'POST'])
 def main_guest():
-    if request.form and request.form['language'] != 'not chosen':
-        lang = request.form['language']
-        test_data = db.get_tests(lang)
-        tests = [str(line[0] + 1) + ' ' + line[4] for line in test_data]
-        return render_template('main.html', chosen = True, 
-                               lang = lang, tasks = tests)
-    else:
-        return render_template('main.html')
+    if request.form:
+        if 'language' in request.form\
+           and request.form['language'] != 'not chosen'\
+           and 'task' not in request.form:
+            lang = request.form['language']
+            test_data = db.get_tests(lang)
+            tests = [str(line[0] + 1) + '. ' + line[4] for line in test_data]
+            return render_template('main.html', chosen = True, 
+                                   lang = lang, tasks = tests)
+        elif 'task' in request.form:
+            task_name = request.form['task']
+            task_id = int(task_name.strip('.'))
+            return render_template('test.html', test_name = task_name)
+    return render_template('main.html')
 
 
 @app.route('/langtests/loggedin')
